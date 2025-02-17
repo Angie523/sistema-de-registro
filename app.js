875de489599679c263ddcd6f31cfadb297e89917
Angie523 <laraangelimer@gmail.com>
2025-02-17 19:03:46 -0300

    creando amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("ejecutando el sistema")
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let input = document.getElementById("amigo");
+let lista = document.getElementById(listaAmigos);
+let amigoAleatorio = document.getElementById("button-add");
+
+
+function sortearAmigo() {
+    if (amigos.length !== 0) {
+       let amigoAleatorio = Math.floor(Math.random() * amigos.length);
+       
+       let amigo = amigos[amigoAleatorio];
+
+       let li = document.createElement('li');
+
+       li.textContent = `Tu amigo secreto es ${amigo}`;
+
+       document.getElementById('resultado').append(li);
+
+       eliminarAmigoDeListaYArray(amigo);
+
+       setTimeout(() => {
+         li.remove();
+        },4000);
+        actualizarBoton();
+    } else {
+      actualizarBoton();
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+}
